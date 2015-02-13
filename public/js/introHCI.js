296633712660ff5f79ba399ceb4164a99c95b93e
vagrant <vagrant@precise32.(none)>
2015-02-13 13:14:16 +0000

    wow

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,9 +14,7 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
-/*
- * Make an AJAX call to retrieve project details and add it in
- */
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -26,13 +24,50 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	console.log(projectID);
+	$.get("/project/"+idNumber,addProject);
+
+	console.log("projectID produced: " + projectID);
+	console.log("/project/" + projectID);
 	console.log("User clicked on project " + idNumber);
 }
+
+function addProject(result){
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' +
+		'<img src=" ' + result['image'] + ' "class="img">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p></a>' +
+		'<p>' + result['summary'] + '</p>';
+
+		$("#project"+result['id'] +" .details").html(projectHTML);
+}
+
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
+
 	console.log("User clicked on color button");
+	var url="/palette"
+	$.get(url,callBack);
+
+
 }
+
+function callBack(result){
+
+	var colorsArray = result['colors'];
+	console.log(colorsArray);
+	var colors = colorsArray['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
